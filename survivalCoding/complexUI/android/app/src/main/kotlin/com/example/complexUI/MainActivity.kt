570a378bea546c8dfa22af6d1f100da75b942e9a
KoreanCow <tyunnon1257@naver.com>
2020-12-15 15:46:37 +0900

    :)

--- survivalCoding/complexUI/android/app/src/main/kotlin/com/example/complexUI/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.complexUI
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- survivalCoding/toDoList/android/app/src/main/kotlin/com/example/toDoList/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.toDoList
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
